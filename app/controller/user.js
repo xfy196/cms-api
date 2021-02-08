e0f90ec789449fc638a3160e668cd1387c31224d
xfy196 <xufuying@outlook.com>
2021-02-08 16:57:21 +0800

    加入用户的增改查

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,29 +1,40 @@
-const {Controller} = require('egg')
-class UserController extends Controller{
+const {
+    Controller
+} = require('egg')
+class UserController extends Controller {
 
     // 查询用户表功能
-    async index(){
-        const {ctx, service} = this
-       let users =  await service.user.list()
-       ctx.body = users
+    async index() {
+        const {
+            ctx,
+            service
+        } = this
+        let users = await service.user.list()
+        ctx.body = users
     }
 
     /**
      * 获取最新的
      */
-    async new(){
-        const {ctx, service} = this
+    async new() {
+        const {
+            ctx,
+            service
+        } = this
         ctx.body = {}
     }
     /**
      * 创建一个用户
      */
-    async create(){
-        const {ctx, service} = this;
+    async create() {
+        const {
+            ctx,
+            service
+        } = this;
         let user = ctx.request.body
         await service.user.create(user)
         ctx.body = {
-            status : 200,
+            status: 200,
             data: {},
             message: "创建成功"
         }
@@ -32,9 +43,14 @@ class UserController extends Controller{
     /**
      * 通过一个id获取数据
      */
-    async show(){
-        const {ctx, service} = this;
-        let {id} = ctx.params
+    async show() {
+        const {
+            ctx,
+            service
+        } = this;
+        let {
+            id
+        } = ctx.params
         let result = await service.user.getUserById(id)
         ctx.body = {
             result
@@ -44,28 +60,52 @@ class UserController extends Controller{
     /**
      * 更新操作
      */
-    async update(){
-        const {ctx, service} = this;
-        ctx.body = {
+    async update() {
+        const {
+            ctx,
+            service
+        } = this;
+        let id = ctx.params.id
+        let user = ctx.request.body
+        user.id = id
+        let {
+            affectedRows
+        } = await service.user.update(user)
 
+        ctx.body = {
+            code: affectedRows === 1 ? 200 : 500,
+            data: {},
+            message: affectedRows === 1 ? "更新成功" : "更新失败"
         }
     }
 
     /**
      * 删除操作 通过id
      */
-    async destroy(){
-        const {ctx, service} = this;
+    async destroy() {
+        const {
+            ctx,
+            service
+        } = this;
+        let id = ctx.params.id
+        let {
+            affectedRows
+        } = await service.user.destroy(id)
         ctx.body = {
-
+            code: affectedRows === 1 ? 200 : 500,
+            data: {},
+            message: affectedRows === 1 ? "删除成功" : "删除失败"
         }
     }
 
     /**
      * get请求的修改操作
      */
-    async edit(){
-        const {ctx, service} = this;
+    async edit() {
+        const {
+            ctx,
+            service
+        } = this;
         ctx.body = {
 
         }
--- app/service/user.js
+++ app/service/user.js
@@ -22,5 +22,22 @@ class UserService extends Service{
     async create(user){
        return await this.app.mysql.insert("cms_user", user)
     }
+    /**
+     * 更新用户信息
+     * @param {*} user 用户实体参数
+     */
+    async update(user){
+        return await this.app.mysql.update("cms_user", user)
+    }
+
+    /**
+     * 通过用户id删除用户
+     * @param {*} id 用户的唯一id
+     */
+    async destroy(id){
+        return await this.app.mysql.delete("cms_user", {
+            id
+        })
+    }
 }
 module.exports = UserService
