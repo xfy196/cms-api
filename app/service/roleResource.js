61feb9fb9fadb042f9205cb1ab209787c6ee600b
xfy196 <xfy196@outlook.com>
2021-03-27 13:57:04 +0800

    update

--- app/service/roleResource.js
+++ app/service/roleResource.js
@@ -15,17 +15,13 @@ class RoleResourceService extends BaseService {
 	async setRoleResource({ role_id, resource_ids }) {
 		try {
 			const { app } = this
-			let { affectedRows } = await app.mysql.query("DELETE FROM role_resource WHERE role_id=?", [role_id])
-			if (!affectedRows) {
-				return false
-			}
+			await app.mysql.query("DELETE FROM role_resource WHERE role_id=?", [role_id])
 			// 循环设置新的权限
 			for (let i = 0; i < resource_ids.length; i++) {
 				let a = await app.mysql.insert("role_resource", {
 					role_id,
 					resource_id: resource_ids[i]
 				})
-				console.log(a)
 			}
 			return true
 		} catch (error) {
