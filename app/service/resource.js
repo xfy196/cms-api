34bbd2823a6f31ae89d108e9f3b44c73039cdf83
xfy196 <xfy196@outlook.com>
2021-04-10 20:01:08 +0800

    update

--- app/service/resource.js
+++ app/service/resource.js
@@ -1,36 +1,42 @@
-const BaseService = require("./base")
+const BaseService = require("./base");
 class ResourceService extends BaseService {
-    constructor(...args) {
-        super(...args)
-        this.entity_table = "cms_resource"
+  constructor(...args) {
+    super(...args);
+    this.entity_table = "cms_resource";
+  }
+  /**
+   *
+   * @returns
+   * 获取系统菜单树的资源
+   */
+  async getResource(userId) {
+    try {
+      const { app } = this;
+      const resources = await app.mysql.query(
+        `
+              SELECT cms_resource.* FROM role_user
+      INNER JOIN role_resource ON role_user.role_id = role_resource.role_id
+      INNER JOIN cms_resource ON role_resource.resource_id = cms_resource.id
+      WHERE role_user.user_id = ? ORDER BY cms_resource.parent_id ASC`,
+        [userId]
+      );
+      // 把得到的结果进行树级菜单处理
+      let map = {};
+      let sourceMenus = [];
+      resources.forEach((resource) => {
+        resource.children = [];
+        map[resource.id] = resource;
+        // 如果你的parent_id为0那么我们认为就把资源放入这一层中
+        if (resource.parent_id === 0) {
+          sourceMenus.push(resource);
+        } else {
+          map[resource.parent_id].children.push(resource);
+        }
+      });
+      return sourceMenus;
+    } catch (error) {
+        throw(error)
     }
-    /**
-     * 
-     * @returns 
-     * 获取系统菜单树的资源
-     */
-    async getResource(userId) {
-        const { app } = this;
-        const resources = await app.mysql.query(`
-        SELECT cms_resource.* FROM cms_user INNER JOIN role_user ON cms_user.id = role_user.user_id
-INNER JOIN role_resource ON role_user.role_id = role_resource.role_id
-INNER JOIN cms_resource ON role_resource.resource_id = cms_resource.id
-WHERE cms_user.id = ${userId}`)
-        // 把得到的结果进行树级菜单处理
-        let map = {}
-        let sourceMenus = []
-        resources.forEach(resource => {
-            resource.children = []
-            map[resource.id] = resource
-            // 如果你的parent_id为0那么我们认为就把资源放入这一层中
-            if(resource.parent_id === 0){
-                sourceMenus.push(resource)
-            }else {
-                map[resource.parent_id].children.push(resource)
-            }
-        })
-        return sourceMenus
-    }
-
+  }
 }
-module.exports = ResourceService
+module.exports = ResourceService;
